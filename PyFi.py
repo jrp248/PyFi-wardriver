c9f5543abe1c0beb12a4bc364e48a53a6ffe7f6d
Jordan Smith <jrp248@wildcats.unh.edu>
2018-02-21 11:57:43 -0500

    Update PyFi.py

--- PyFi.py
+++ PyFi.py
@@ -3,8 +3,9 @@ import scipy as sp
 import matplotlib.pyplot as plt
 import csv
 
-with open('WigleWifi_2018_0220.csv','rb') as f:
-   reader = csv.reader(f)
-   complete = list(reader)
-  
-print(complete)
+# Reading entire file first.
+
+with open('WigleWifi_2018_0220.csv','rt', encoding='utf8') as f:
+	fiReader = csv.reader(f, delimiter=',')
+	for row in fiReader:
+		print(', '.join(row))
