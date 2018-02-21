9eaded0fd4c1039c4efdbd2828cae5f2c12323f2
Jordan Smith <jrp248@wildcats.unh.edu>
2018-02-21 12:04:35 -0500

    Update PyFi.py

--- PyFi.py
+++ PyFi.py
@@ -2,10 +2,13 @@ import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
 import csv
+import os
 
-# Reading entire file first.
+script_dir = os.path.dirname(__file__)
+csv_path = os.path.join(script_dir, '/CSV/WigleWifi_2018_0220.csv')
 
-with open('WigleWifi_2018_0220.csv','rt', encoding='utf8') as f:
+
+with open('CSV/WigleWifi_2018_0220.csv','rt', encoding='utf8') as f:
 	fiReader = csv.reader(f, delimiter=',')
 	for row in fiReader:
 		print(', '.join(row))
