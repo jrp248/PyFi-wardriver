9c63dc23dd020d4a4ffea96ef8ab0a8ecad5eb08
Jordan Smith <jrp248@wildcats.unh.edu>
2018-02-21 17:20:58 -0500

    Modified reading of CSV Files to include correct header of columns for Panda package dataframe.

--- PyFi.py
+++ PyFi.py
@@ -1,14 +1,26 @@
 import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
-import csv
-import os
+import csv, os
+import collections as cl
+import pandas as pd
+
 
 script_dir = os.path.dirname(__file__)
 csv_path = os.path.join(script_dir, '/CSV/WigleWifi_2018_0220.csv')
 
+with open('CSV/WigleWifi_2018_0220.csv','rt') as f:
+	fiReader = csv.reader(f,delimiter=',')
+	fiList = list(fiReader)
 
-with open('CSV/WigleWifi_2018_0220.csv','rt', encoding='utf8') as f:
-	fiReader = csv.reader(f, delimiter=',')
-	for row in fiReader:
-		print(', '.join(row))
+
+# panda conversion
+df = pd.read_csv('CSV/WigleWifi_2018_0220.csv',delimiter=',',skiprows=0,header=1)
+
+MAC = df.MAC	# passes all MAC addresses to a single variable.
+SSID = df.SSID
+
+countMAC = cl.Counter(MAC)		# stores recurring addresses.
+countSSID = cl.Counter(SSID)	# store recurring network or access point names.
+
+print(countSSID)				# figure out how to gain row indeces from panda dataframes.
