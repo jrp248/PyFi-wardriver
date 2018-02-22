d2ef9932c9baa1b85d5baf25b226c70a5c3a2c05
Jordan <jrp248@wildcats.unh.edu>
2018-02-22 00:54:59 -0500

    Found a command for use in Matplotlib.pyplot usage.

--- PyFi.py
+++ PyFi.py
@@ -1,6 +1,5 @@
 import numpy as np
 import scipy as sp
-import matplotlib.pyplot as plt
 import csv, os
 import collections as cl
 import pandas as pd
@@ -23,4 +22,5 @@ SSID = df.SSID
 countMAC = cl.Counter(MAC)		# stores recurring addresses.
 countSSID = cl.Counter(SSID)	# store recurring network or access point names.
 
-print(countSSID)				# figure out how to gain row indeces from panda dataframes.
+print(MAC)
+MACsum = MAC.cumsum()
